8b4b67b28bc4abb8a9719e32cd8538f630b84e91
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-06 00:18:16 -0700

    Breadcrumbs

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { breakStatement } from '@babel/types';
 
 function RenderDirectoryItem( {campsite} ) {
     return (
@@ -27,6 +28,16 @@ function Directory(props) {
 
         return (
             <div className="container" >
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Directory</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Directory</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     {directory}
                 </div>
