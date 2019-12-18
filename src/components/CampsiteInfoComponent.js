887203e3839b9f09bbe7f1d327f209475f1cd154
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-17 21:28:43 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { Link, Route } from 'react-router-dom';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
+const required = val => val && val.length;
 
 class CommentForm extends Component {
     constructor(props) {
@@ -30,8 +31,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
     
     render() {
@@ -74,6 +75,7 @@ class CommentForm extends Component {
                                         placeholder="Your Name"
                                         className="form-control"
                                         validators={{
+                                            required,
                                             minLength: minLength(2),
                                             maxLength: maxLength(15)
                                         }}
@@ -85,6 +87,7 @@ class CommentForm extends Component {
                                         show="touched"
                                         component="div"
                                         messages={{
+                                            required: "Required",
                                             minLength: 'Must be at least 2 characters.',
                                             maxLength: 'Must be 15 characters or less.'
                                         }}
@@ -132,7 +135,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             
             return (
@@ -148,7 +151,7 @@ function RenderComments({comments}) {
                             </div>
                         )
                     })}
-                    <CommentForm/>
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
                 </div>
             )
         }
@@ -175,7 +178,11 @@ function CampsiteInfo(props) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                            comments={props.comments} 
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
                     </div>
                 </div>
             );
