21eb43841e5ca5a00e85b140fa0edc64fc3f791b
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-20 21:33:13 -0700

    Fetch from Server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,7 +1,7 @@
 import * as ActionTypes from './ActionTypes';
-import { CAMPSITES } from '../shared/campsites';
+import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (campsiteId, rating, author, text) => ({
+export const addComments = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
         campsiteId: campsiteId,
@@ -15,9 +15,9 @@ export const fetchCampsites = () => dispatch => {
 
     dispatch(campsitesLoading());
 
-    setTimeout(() => {
-        dispatch(addCampsites(CAMPSITES));
-    }, 2000);
+    return fetch(baseUrl + 'campsites')
+        .then(response => response.json())
+        .then(campsites => dispatch(addCampsites(campsites)));
 };
 
 export const campsitesLoading = () => ({
@@ -32,4 +32,43 @@ export const campsitesFailed = errMess => ({
 export const addCampsites = campsites => ({
     type: ActionTypes.ADD_CAMPSITES,
     payload: campsites 
+});
+
+export const fetchComments = () => dispatch => {
+    return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComment(comments)));
+};
+
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+
+export const addComment = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromotions = () => dispatch => {
+
+    dispatch(promotionsLoading());
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)));
+};
+
+export const promotionsLoading = () => ({
+    type: ActionTypes.PROMOTIONS_LOADING
+});
+
+export const promotionsFailed = errMess => ({
+    type: ActionTypes.PROMOTIONS_FAILED,
+    payload: errMess
+});
+
+export const addPromotions = promotions => ({
+    type: ActionTypes.ADD_PROMOTIONS,
+    payload: promotions 
 });
